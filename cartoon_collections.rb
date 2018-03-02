c5a84df377af84ca7c442b7ddb3f7caca01b0f4b
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-03-02 02:11:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,15 @@ def long_planeteer_calls(array)# code an argument here
 end
 
 def find_the_cheese(array)# code an argument here
-
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  if array.include?(cheese_types[0])
+    return cheese_types[0]
+  elsif array.include?(cheese_types[1])
+    return cheese_types[1]
+  else array.include?(cheese_types[2])
+    return cheese_types[2]
+  end
+    
 end
