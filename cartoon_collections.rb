22081dd44517744a50beadb176329914bb7cb2b1
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-03-02 01:21:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,12 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.include?(word.length > 3)
+  array.each{|name| array.include?(name.length > 3)}
+
+  array.any? do |word|
+    word.length > 3
+    array
+  end
 
   # Your code here
 end
